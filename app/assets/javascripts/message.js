aa1e5dc99f8c7fd1f46293224c4a61eab8c7dbbe
kissy0117 <yk.supercell@gmail.com>
2020-01-25 18:15:53 +0900

    非同期通信の実装#1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,65 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message-upper">
+            <div class="message-upper__name">
+              ${message.user_name}
+            </div>
+            <div class="message-upper__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-under">
+            <p class="message-under__text">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message-upper">
+            <div class="message-upper__name">
+              ${message.user_name}
+            </div>
+            <div class="message-upper__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message-under">
+            <p class="message-under__text">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit',function(e){
-    console.log('hoge');
-    e.preventDefault()
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight})
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
   });
+  })
 });
